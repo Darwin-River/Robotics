f174c63fdd71f611a3bc62e0d24806fd5d91bd26
Ex Machinis <david.rozak@darwinriver.com>
2024-01-06 20:04:20 -0500

    Corrected error in which ino file duplicated help file.

--- Libraries/Code_Library/LineRacer/StateMachine.ino
+++ Libraries/Code_Library/LineRacer/StateMachine.ino
@@ -1,38 +1,105 @@
-#pragma once
+#include "StateMachine.h"
 
-#define MAX_STATES 20
+#define STOPPED  0
+#define STARTING 1
+#define RUNNING  2
+#define STOPPING 3
 
-class StateMachine
+StateMachine::StateMachine() {}
+
+void StateMachine::addState(int _state, int _trigger, int _trueDestination, int _trueActions, int _falseDestination, int _falseActions)
 {
-  private:
-    int status = 0;
+  triggers[_state] = _trigger;
+  trueActions[_state] = _trueActions;
+  falseActions[_state] = _falseActions;
+  trueDestinations[_state] = _trueDestination;
+  falseDestinations[_state] = _falseDestination;
+}
 
-    int triggers[MAX_STATES];
-    int trueActions[MAX_STATES];
-    int falseActions[MAX_STATES];
-    int trueDestinations[MAX_STATES];
-    int falseDestinations[MAX_STATES];
+void StateMachine::onStart(int _startActions)
+{
+  startActions = _startActions;
+}
 
-  	int currentState;
-    int currentEvents;
-  	int currentActions;
-    int startActions;
-    int stopActions;
+void StateMachine::onStop(int _stopActions)
+{
+  stopActions = _stopActions;
+}
 
-    int eventCount;
-    int actionCount;
-  
-  public:
-  	StateMachine();
-    void addState(int _state, int _trigger, int _trueDestination, int _trueActions, int _falseDestination,  int _falseActions);
-    void onStart(int _startActions);
-    void onStop(int _stopActions);
-    int createEventHandle();
-    int createActionHandle(); 
-    void logEvent(int _event, bool _status);
-    void evaluate();
-    void start();
-    void stop();
-    int getState();
-    bool requests(int _action);
-};
+int StateMachine::createEventHandle()
+{
+  eventCount ++;
+  return bit(eventCount - 1);
+}
+
+int StateMachine::createActionHandle()
+{
+  actionCount ++;
+  return bit(actionCount - 1);
+}
+
+void StateMachine::logEvent(int _event, bool _status)
+{
+  if (_status) currentEvents = currentEvents | _event;
+}
+
+void StateMachine::evaluate()
+{
+  switch(status)
+  {
+    case STARTING:
+      Serial.println("Starting");
+      currentActions = startActions;
+      currentState = 0;
+      status=RUNNING;
+      break;
+    case RUNNING:
+      Serial.println(currentState);
+      if ((triggers[currentState] & currentEvents) > 0)
+      {
+        currentActions = trueActions[currentState];
+        currentState = trueDestinations[currentState];
+      }
+      else
+      {
+        currentActions = falseActions[currentState];
+        currentState = falseDestinations[currentState];
+      }
+      break;
+    case STOPPING:
+      Serial.println("Stopping");
+      status = STOPPED;
+      currentActions = stopActions;
+      currentState = 0;
+      status=STOPPED;
+      break;
+    case STOPPED:
+      // Serial.println("Stopped");
+      currentActions = 0;
+      currentState = 0;
+      break;
+  }
+  currentEvents = 0;
+}
+
+void StateMachine::start()
+{
+  Serial.println("Told to start.");
+  status = STARTING;
+}
+
+void StateMachine::stop()
+{
+  Serial.println("Told to stop.");
+  status = STOPPING;
+}
+
+int StateMachine::getState()
+{
+  return currentState;
+}
+
+bool StateMachine::requests(int _action)
+{
+  return (currentActions & _action) > 0;
+}
